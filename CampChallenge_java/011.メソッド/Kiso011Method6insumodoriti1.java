e12980574d00d15daf3b3cc16bc5c310baf62810
Ryuichi Hashimoto <durrrr31pkmj@yahoo.co.jp>
2018-05-29 17:45:24 +0900

    引数、戻り値１を完成させました。

---
+++ CampChallenge_java/011.メソッド/Kiso011Method6insumodoriti1.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author guest1Day
+ */
+@WebServlet(urlPatterns = {"/Kiso011Method6insumodoriti1"})
+public class Kiso011Method6insumodoriti1 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+    String[] profile(String moji){
+            String[][] prof = {{"ID1","1995年1月6日生","千葉県船橋市"},
+                              {"ID2","1995年3月25日生","神奈川県横浜市"},
+                              {"ID3","1994年5月10日生","東京都町田市"}};
+                for(int i=0; i<=prof.length; i++){
+
+               if(prof[i][0].equals(moji)){
+                   return prof[i];
+               }
+               }
+           return null;
+    }
+    
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+           
+
+            String[] profe = profile("ID2");
+            
+            for(int i=0; i<=profe.length; i++){
+                if(profe[i].indexOf("ID") != -1){
+                    continue;
+                }else{out.print(profe[i]);
+                }
+            }
+            }
+        }
+            
+            
+   
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
