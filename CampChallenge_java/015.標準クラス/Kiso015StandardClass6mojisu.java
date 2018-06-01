1677e56dceb2b9f75c9a6259196539ec47fc61d7
Ryuichi Hashimoto <durrrr31pkmj@yahoo.co.jp>
2018-06-01 18:05:30 +0900

    「文字数の取得」を改めて完成させました。

--- CampChallenge_java/015.標準クラス/Kiso015StandardClass6mojisu.java
+++ CampChallenge_java/015.標準クラス/Kiso015StandardClass6mojisu.java
@@ -9,5 +9,13 @@
  * @author guest1Day
  */
 public class Kiso015StandardClass6mojisu {
+    public static void main(String[] args){
+        
+        String mailad = "durrrrrrrrrrrrrrrrrrrrrrrrrrrrrr@yahoo.co.jp";
+        
+        String ma = mailad.substring(mailad.indexOf("@"));
+        
+        System.out.print(ma.length()+"文字");
+    }
     
 }
